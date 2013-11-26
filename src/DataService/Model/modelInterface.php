f003b71a75a54d6efb5a2769e42d0df63285f70d
aurelienPligot <aurelien.pligot@yahoo.es>
2013-11-26 16:33:51 +0100

    Modification of model interface

---
+++ src/DataService/Model/modelInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DataService\Model;
+
+interface ModelInterface
+{
+    public function findBy(Property $property);
+
+    public function find(Model $model);
+
+    public function findAll();
+
+    public function add(Model $model);
+
+    public function remove(Model $model);
+}
