cb35d13a68286ec77e5b72cfb8023e9fce417c0b
nicobascon <nico.bascon@live.fr>
2013-11-29 16:31:11 +0100

    Validate model and property object

--- app/data-service.php
+++ app/data-service.php
@@ -5,12 +5,18 @@ $app = require_once __DIR__ . '/config/config.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+
+require_once __DIR__ . '/../src/DataService/Model/modelClass.php';
+require_once __DIR__ . '/../src/DataService/Model/propertyClass.php';
+
 $app->get('/', function () {
     return 'Hello world!';
 });
 
+// Request created for testing
 $request = Request::create('','',array(), array(), array(), array(),
-    '{"model": { "properties": {"firstName":{"type":"string", "description":"Nom de la personne"}}, "metadata":{"name":{"value":"people", "type":"string"}, "description": { "value":"Des personnes", "type":"string"}}}}');
+    '{"model": { "properties": {"firstName":{"type":"string"}, "lastName":{"type":"string", "description":"Nom de la personne"}, "age":{"type":"integer"}}, "metadata":{"name":{"value":"2132", "type":"string"}, "description": { "value":"Des personnes", "type":"string"}}}}');
 $request->headers->set('Accept', array('application/json'));
 $request->headers->set('content_type', array('application/json'));
 
@@ -21,16 +27,11 @@ $before = function () use ($request) {
     }
 };
 
-/*
-$description
-$name
-$filters
-$properties
-*/
 $app->post('/models', function () use ($request, $app) {
     $model = json_decode($request->getContent());
     $right = false;
     if ($model != null){
+        $filters = null;
         $attributeList = get_object_vars($model);
         $existModel = false;
         $existProperties = false;
@@ -41,7 +42,6 @@ $app->post('/models', function () use ($request, $app) {
                 $existModel = true;
             }
         }
-
         if ($existModel) {
             if (is_object($model->{'model'})) {
                 $attributeList = get_object_vars($model->{'model'});
@@ -103,67 +103,82 @@ $app->post('/models', function () use ($request, $app) {
                 foreach ($attributeList as $key => $value) {
                     if (is_object($model->{'model'}->{'properties'}->$key)) {
                         $attributeList = get_object_vars($model->{'model'}->{'properties'}->$key);
-                        for ($i = 0 ; $i < count($attributeList) ; $i ++) {
-                            foreach ($attributeList as $key => $value) {
-                                $name = $key;
-                                $typeProperties = '';
-                                $descriptionProperties = '';
-                                $patternExist = false;
-                                $formatExist = false;
+                        $nameProperty = $key;
+                        $typeProperty = '';
+                        $descriptionExist = false;
+                        $patternExist = false;
+                        $formatExist = false;
+                        foreach ($attributeList as $key => $value) {
 
-                                if (strcmp($key, 'type') == 0) {
-                                    $typeProperties = $value;
-                                }
-                                if (strcmp($key, 'description') == 0) {
-                                    $descriptionProperties = $value;
-                                }
-                                if (strcmp($key, 'pattern') == 0) {
-                                    $patternExist = true;
-                                    $patternProperties = $value;
-                                }
-                                if (strcmp($key, 'format') == 0) {
-                                    $formatExist = true;
-                                    $formatProperties = $value;
-                                }
+                            if (strcmp($key, 'type') == 0) {
+                                $typeProperty = $value;
+                            }
+                            if (strcmp($key, 'description') == 0) {
+                                $descriptionExist = true;
+                                $descriptionProperty = $value;
+                            }
+                            if (strcmp($key, 'pattern') == 0) {
+                                $patternExist = true;
+                                $patternProperty = $value;
+                            }
+                            if (strcmp($key, 'format') == 0) {
+                                $formatExist = true;
+                                $formatProperty = $value;
                             }
                         }
-                        if ($patternExist && $formatExist) {
-                            $property = array(
-                                'name' => $name,
-                                'type' => $typeProperties,
-                                'description' => $descriptionProperties,
-                                'pattern' => $patternProperties,
-                                'format' => $formatProperties
-                            );
+                        if ($descriptionExist && $patternExist && $formatExist) {
+                            $property = new Property($nameProperty, $typeProperty, $descriptionProperty, $patternProperty, $formatProperty);
                         }
                         else {
-                            if ($patternExist) {
-                                $property = array(
-                                    'name' => $name,
-                                    'type' => $typeProperties,
-                                    'description' => $descriptionProperties,
-                                    'pattern' => $patternProperties
-                                );
+                            if ($descriptionExist) {
+                                $property = new Property($nameProperty, $typeProperty, $descriptionProperty);
                             }
                             else {
-                                if ($formatExist) {
-                                    $property = array(
-                                        'name' => $name,
-                                        'type' => $typeProperties,
-                                        'description' => $descriptionProperties,
-                                        'format' => $formatProperties
-                                    );
+                                if ($patternExist) {
+                                    $property = new Property($nameProperty, $typeProperty, null, $patternProperty);
                                 }
                                 else {
-                                    $property = array(
-                                        'name' => $name,
-                                        'type' => $typeProperties,
-                                        'description' => $descriptionProperties
-                                    );
+                                    if ($formatExist) {
+                                        $property = new Property($nameProperty, $typeProperty, null, null, $formatProperty);
+                                    }
+                                    else {
+                                        if ($descriptionExist && $patternExist) {
+                                            $property = new Property($nameProperty, $typeProperty, $descriptionProperty, $patternProperty);
+                                        }
+                                        else {
+                                            if ($descriptionExist && $formatExist) {
+                                                $property = new Property($nameProperty, $typeProperty, $descriptionProperty, null, $formatProperty);
+                                            }
+                                            else {
+                                                if ($patternExist && $formatExist) {
+                                                    $property = new Property($nameProperty, $typeProperty, null, $patternProperty, $formatProperty);
+                                                }
+                                                else {
+                                                    $property = new Property($nameProperty, $typeProperty);
+
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }
-                        array_push($properties, $property);
+                        $errors = $app['validator']->validate($property);
+                        if (count($errors) > 0) {
+                            $right = true;
+                            echo 'Properties errors: <br/>';
+                            foreach ($errors as $error) {
+                                echo $error->getPropertyPath().' '.$error->getMessage().'<br/>';
+                            }
+                            echo '<br/>';
+                        }
+
+                        if (count($errors) > 0) {
+                            $right = true;
+                        }
+                        else {
+                            array_push($properties, $property);
+                        }
                     }
                     else {
                         $right = true;
@@ -210,9 +225,25 @@ $app->post('/models', function () use ($request, $app) {
     if ($right || !$existNameValue) {
         return new Response('Json data badly written', 400);
     }
-    return new Response('New model created', 200);
+    else {
+        $model = new Model($name, $description, $properties, $filters);
+        $errors = $app['validator']->validate($model);
+
+        if (count($errors) > 0) {
+            echo 'Model errors: <br/>';
+            foreach ($errors as $error) {
+                echo $error->getPropertyPath().' '.$error->getMessage().'<br/>';
+            }
+            echo '<br/>';
+            return new Response('Json data badly written', 400);
+        }
+        else {
+            return new Response('Model '.$name.' created', 200);
+        }
+    }
 })
-    ->before($before)
-    ->method('GET|POST');
+->before($before)
+->method('GET|POST');
+
 
 return $app;
