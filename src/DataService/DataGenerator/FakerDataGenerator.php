dadcb572b2833f47502eb73304e988539ecd53d2
nicobascon <nico.bascon@live.fr>
2013-12-17 16:40:41 +0100

    Create FakerDataGenerator

---
+++ src/DataService/DataGenerator/FakerDataGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DataService\DataGenerator;
+
+use DataService\Model\Model;
+
+class FakerDataGenerator implements DataGeneratorInterface
+{
+    public function generateCollection(Model $model)
+    {
+
+    }
+}
