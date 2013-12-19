fc424f8dc5f623acbca98b5b2fc4b31b7b2691c1
nicobascon <nico.bascon@live.fr>
2013-12-19 14:31:22 +0100

    Add generate email with pattern

--- src/DataService/DataGenerator/FakerDataGenerator.php
+++ src/DataService/DataGenerator/FakerDataGenerator.php
@@ -22,20 +22,29 @@ class FakerDataGenerator implements DataGeneratorInterface
         $arrayData = array();
         for ($i = 0 ; $i < count($properties) ; $i ++) {
             $valueProperty = $this->generateData($properties[$i]->getName(), $properties[$i]->getType(),
-                                                 $properties[$i]->getPattern(), $properties[$i]->getFormat());
+                                                 $properties[$i]->getPattern(), $properties[$i]->getFormat(), $arrayData);
             $arrayData[$properties[$i]->getName()] = $valueProperty;
         }
 
         return $arrayData;
     }
 
-    private function generateData($name, $type, $pattern, $format)
+    private function generateData($name, $type, $pattern, $format, array $arrayData)
     {
         if ($type === 'string' && is_null($pattern) && is_null($format)) {
             return $this->generateSimpleDataString($name);
         }
         if ($type === 'number' && is_null($pattern) && is_null($format)) {
             return $this->faker->randomNumber($nbDigits = NULL);
+        }
+        if ($type === 'email' && is_null($pattern) && is_null($format)) {
+            return $this->faker->safeEmail;
+        }
+        if (!is_null($pattern) && is_null($format)) {
+            return $this->generateSimpleDataWithPattern($name, $type, $pattern, $arrayData);
+        }
+        if (is_null($pattern) && !is_null($format)) {
+            return $this->generateSimpleDataWithFormat($name, $type, $format);
         }
 
     }
@@ -68,4 +77,46 @@ class FakerDataGenerator implements DataGeneratorInterface
         }
     }
 
+    private function generateSimpleDataWithPattern($name, $type, $pattern, array $arrayData)
+    {
+        if ($name === 'email' || $type === 'email') {
+            $splitEmailPattern = explode("@", $pattern);
+            $splitPrefixEmailPattern = explode(".", $splitEmailPattern[0]);
+            $prefixMail = '';
+            if (count($splitPrefixEmailPattern) < 2) {
+                $prefixMail = $this->verifyPatternVariable($splitPrefixEmailPattern[0], $arrayData);
+            } else {
+                $finalMail = (count($splitPrefixEmailPattern) > 2 ? 'error' : null);
+                if ($finalMail === 'error') {
+                    return 'error';
+                }
+                $count = 0;
+                foreach ($splitPrefixEmailPattern as $value) {
+                    $prefixMail .= ($count === 1) ? '.' : '';
+                    $prefixMail .= $this->verifyPatternVariable($value, $arrayData);
+                    $count ++;
+                }
+            }
+
+            return $prefixMail.'@'.$splitEmailPattern[1];
+        }
+
+        return $this->faker->safeEmail;
+    }
+
+    private function generateSimpleDataWithFormat($name, $type, $format)
+    {
+        return "Not Yet Implemented";
+    }
+
+    private function verifyPatternVariable($variable, array $arrayData)
+    {
+        if (substr($variable, 0, 1) != '{' || substr($variable, -1) != '}') {
+            return 'error';
+        }
+        $variable = substr($variable, 1, -1);
+        if (array_key_exists($variable, $arrayData)) {
+            return strtolower($arrayData[$variable]);
+        }
+    }
 }
